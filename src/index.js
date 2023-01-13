5d8a3b8a4495d62acd3afb09b2f461d63136f7ed
Qwerton <Qwerton@users.noreply.github.com>
2023-01-13 16:18:34 +0300

    final script version

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ module.exports = function toReadable (number) {
 
   let strNum = String(number);
   let strNumLength = strNum.length;
-  let oneDigit;
-  let twoDigit;
-  let threeDigit;
+  let oneDigit = '';
+  let twoDigit = '';
+  let threeDigit = '';
 
   let arr = ['','one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
   let arr2 = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
@@ -13,12 +13,9 @@ module.exports = function toReadable (number) {
   function digitCalc(i) {
     if (+strNum[i] === 0) {
       oneDigit = arr[+strNum[i+1]];
-      twoDigit = '';
 
     } else if (+strNum[i] === 1) {
-        oneDigit = '';
         twoDigit = arr2[+strNum[i+1]];
-        threeDigit = '';
 
     } else if ((+strNum[i] >= 2 && +strNum[i] <= 9)) {
       oneDigit = arr[+strNum[i+1]];
@@ -27,12 +24,9 @@ module.exports = function toReadable (number) {
   }
 
   if (strNumLength === 1) {
-    twoDigit = '';
-    threeDigit ='';
     oneDigit = arr[+strNum[0]];
 
   } else if (strNumLength === 2) {
-     threeDigit ='';
      digitCalc(0);
 
   } else if (strNumLength === 3) {
